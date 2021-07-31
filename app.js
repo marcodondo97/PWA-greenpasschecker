c778fad3922581b1f3f59709f8498bbc353fa772
Marco Dondo <dondo.marco.itsv@gmail.com>
2021-07-31 11:11:22 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+/*modules require */
+const express = require("express")
+const path = require("path");
+const http = require('http');
+const https = require('https');
+const app = express();
+const fs = require('fs');
+const utf8 = require('utf8');
+const base45 = require('base45');
+const cbor = require('cbor');
+const pako = require('pako');
+const ejs = require('ejs');
+const bodyParser = require("body-parser");
+
+
+/*certificate declaration */
+const httpsOptions = {
+  key: fs.readFileSync('privatekey.pem'),
+  cert: fs.readFileSync('certificate.pem')
+};
+
+/*file declaration */
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'public/views'));
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+
+/*function request */
+app.post("/request", (req, res) => {
+  const cod = (req.body.name);
+  const greenpassBody = cod.substr(4);
+  // base45 encoded
+  const decodedData = base45.decode(greenpassBody);
+  //unzip
+  const output = pako.inflate(decodedData);
+  const results = cbor.decodeAllSync(output);
+  [headers1, headers2, cbor_data, signature] = results[0].value;
+  const greenpassData = cbor.decodeAllSync(cbor_data);
+  let decod = (JSON.stringify(greenpassData[0].get(-260).get(1), null, 2));
+  res.render('print', { decod: decod });
+});
+
+
+
+
+
+
+/*server setup */
+http.createServer(app).listen(80);
+https.createServer(httpsOptions, app).listen(443);
